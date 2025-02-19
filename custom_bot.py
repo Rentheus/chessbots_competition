eed58183c14f3b4616bfbf293c9319a88eb635a7
Silas Kraume <silas.kraume1552@gmail.com>
2025-02-19 20:10:16 +0100

    fix infinity

--- custom_bot.py
+++ custom_bot.py
@@ -5,7 +5,6 @@ create your own bot here
 import random
 import math
 import chess
-from functools import lru_cache
 
 def bad_bot(shared, board: chess.Board):
     """
@@ -42,19 +41,17 @@ CHESS_VALUES = {
     chess.QUEEN:  9,
     chess.KING:   0,
 }
-
-lru_cache(maxsize=250)
-def legal_moves(board: chess.Board) -> list:
-    return list(board.legal_moves)
-
-
+M_DEPTH = 3
+M_INF = 999
 
 def evaluate_board(board: chess.Board, color: chess.Color) -> int:
     score = 0
 
     for piece_type, value in CHESS_VALUES.items():
-        score += len(board.pieces(piece_type, color)) * value
-        score -= len(board.pieces(piece_type, not color)) * value
+        score += (
+            len(board.pieces(piece_type, color)) - \
+                len(board.pieces(piece_type, not color))
+        ) * value
 
     return score
 
@@ -62,15 +59,15 @@ def mm(board: chess.Board, depth, alpha, beta, max_p, color):
     if board.is_game_over():
         result = board.result()
         if result == "1-0":
-            return math.inf if color == chess.WHITE else -math.inf
+            return (M_INF if color == chess.WHITE else -M_INF) * depth
         if result == "0-1":
-            return math.inf if color == chess.BLACK else -math.inf
+            return (M_INF if color == chess.BLACK else -M_INF) * depth
         return 0
-    if not depth:
+    if depth == 1:
         return evaluate_board(board, color)
 
     if max_p:
-        m_eval = -math.inf
+        m_eval = -M_INF
         for move in board.legal_moves:
             board.push(move)
             c_eval = mm(board, depth - 1, alpha, beta, False, color)
@@ -80,7 +77,7 @@ def mm(board: chess.Board, depth, alpha, beta, max_p, color):
             if beta <= alpha:
                 break
         return m_eval
-    m_eval = math.inf
+    m_eval = M_INF
     for move in board.legal_moves:
         board.push(move)
         c_eval = mm(board, depth - 1, alpha, beta, True, color)
@@ -92,16 +89,16 @@ def mm(board: chess.Board, depth, alpha, beta, max_p, color):
     return m_eval
 
 def fick_sie_alle_bot(shared, board: chess.Board) -> None:
-    best_score = -math.inf
+    best_score = -M_INF
     color = board.turn
 
     for move in board.legal_moves:
         board.push(move)
-        score = mm(board, 2, -math.inf, math.inf, False, color) + random.random()/2
+        score = mm(board, M_DEPTH, -M_INF, M_INF, False, color) + random.random()/2
         if board.is_check():
             score += 0.5
         board.pop()
-
+        # print(move, score)
         mmove = chess.Move.from_uci(str(move))
         if board.piece_at(mmove.from_square).symbol() == \
             chess.Piece(chess.KING, board.turn).symbol():
